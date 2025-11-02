cc3d6a60ae1ce3d5ea756ca04af4f32169aa2cb9
ArefehHamidi <106145596+ArefehHamidi@users.noreply.github.com>
2025-11-02 14:15:55 +0330

    Add files via upload

---
+++ contact/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'contact'
+
+urlpatterns = [
+    path('' , views.send_email , name='send_mail'),
+    path('success/' , views.success , name='success')
+
+
+]
